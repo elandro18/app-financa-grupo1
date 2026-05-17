cf278c50d60c72da747e1ff4ecb163afa93d4b68
Vanessa Ponte <vanessaponte@gmail.com>
2026-05-17 11:31:19 -0300

    Feat: Update 404 Not Found page with new image and improved layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,13 +4,13 @@ export default function NotFound() {
     return (
         <div className="flex h-screen items-center justify-center">
             <Image 
-                src="/404.png"
+                src="/404-new.png"
                 width={400}
                 height={400}
                 alt="404"
                 className=""
             />
-            <h1 className='font-bold'>Página não encontrada</h1>
+            <h1 className='text-center text-3xl font-semibold leading-normal my-8'>Página não encontrada</h1>
         </div>
     )
 }
